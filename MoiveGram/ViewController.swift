7934ae1dcf023e8a21a4d1105d84a587fecb2e08
skkim125 <kthanks125@gmail.com>
2024-06-04 16:17:51 +0900

    Initial Commit

---
+++ MoiveGram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoiveGram
+//
+//  Created by 김상규 on 6/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
