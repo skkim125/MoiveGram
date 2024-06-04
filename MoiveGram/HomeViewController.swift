01364fd064222db091d35f51ae152f89d58230f1
skkim125 <kthanks125@gmail.com>
2024-06-05 02:10:43 +0900

    MovieGram Project - Code Based UI: SnapKit으로 Auto Layout 설정 - push 화면 전환을 위해 SceneDelegate에서 ViewController를 NavigationController로 설정 - 영화 포스터 이미지 삽입을 위해 더미 데이터 추가

---
+++ MoiveGram/HomeViewController.swift
@@ -0,0 +1,169 @@
+//
+//  HomeViewController.swift
+//  MoiveGram
+//
+//  Created by 김상규 on 6/4/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let mainImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "극한직업")
+        view.backgroundColor = .lightGray
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        
+        return view
+    }()
+    
+    let subImageView1: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "명량")
+        view.backgroundColor = .blue
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        
+        return view
+    }()
+    
+    let subImageView2: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "도둑들")
+        view.backgroundColor = .black
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        
+        return view
+    }()
+    
+    let subImageView3: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "베테랑")
+        view.backgroundColor = .red
+        view.contentMode = .scaleAspectFill
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        
+        return view
+    }()
+    
+    let nowHotContentsLabel: UILabel = {
+        let view = UILabel()
+        view.layer.cornerRadius = 8
+        view.text = "지금 뜨는 콘텐츠"
+        view.font = .systemFont(ofSize: 16)
+        view.textColor = .white
+        
+        return view
+    }()
+    
+    let watchButton: UIButton = {
+        let view = UIButton()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 8
+        view.setTitle("재생", for: .normal)
+        view.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        view.imageView?.contentMode = .scaleAspectFit
+        view.setTitleColor(.black, for: .normal)
+        
+        
+        return view
+    }()
+    
+    let saveButton: UIButton = {
+        let view = UIButton()
+        view.backgroundColor = .darkGray
+        view.layer.cornerRadius = 8
+        
+        view.setTitle("내가 찜한 리스트", for: .normal)
+        view.setImage(UIImage(systemName: "plus"), for: .normal)
+        view.imageView?.contentMode = .scaleAspectFit
+        view.setTitleColor(.white, for: .normal)
+        
+        return view
+    }()
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .gray
+        navigationItem.title = "Harvey님"
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.barTintColor = .white
+        
+        configureHierarchy()
+        configureLayout()
+    }
+
+    
+    
+    func configureHierarchy() {
+        let viewArray = [mainImageView, subImageView1, subImageView2, subImageView3, nowHotContentsLabel, watchButton, saveButton]
+        
+        for item in viewArray {
+            view.addSubview(item)
+        }
+    }
+    
+    func configureLayout() {
+        mainImageView.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.height.equalTo(480)
+        }
+        
+        nowHotContentsLabel.snp.makeConstraints { make in
+            make.top.equalTo(mainImageView.snp.bottom).offset(8)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(12)
+            make.height.equalTo(32)
+        }
+        
+        subImageView1.snp.makeConstraints { make in
+            make.top.equalTo(nowHotContentsLabel.snp.bottom).offset(8) //
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(20) //
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(8) //
+        }
+        
+        subImageView2.snp.makeConstraints { make in
+            make.centerX.equalTo(mainImageView.snp.centerX)
+            make.top.equalTo(nowHotContentsLabel.snp.bottom).offset(8) //
+            make.leading.equalTo(subImageView1.snp.trailing).offset(8)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(8) //
+            make.width.equalTo(subImageView1.snp.width)
+        }
+        
+        subImageView3.snp.makeConstraints { make in
+            make.top.equalTo(nowHotContentsLabel.snp.bottom).offset(8)
+            make.leading.equalTo(subImageView2.snp.trailing).offset(8)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).inset(8) //
+        }
+        
+        watchButton.snp.makeConstraints { make in
+            make.bottom.equalTo(mainImageView.snp.bottom).inset(12)
+            make.leading.equalTo(mainImageView.snp.leading).inset(12)
+            make.height.equalTo(44)
+        }
+        
+        saveButton.snp.makeConstraints { make in
+            make.bottom.equalTo(mainImageView.snp.bottom).inset(12)
+            make.leading.equalTo(watchButton.snp.trailing).offset(12)
+            make.trailing.equalTo(mainImageView.snp.trailing).inset(12)
+            make.height.equalTo(44)
+            make.width.equalTo(watchButton.snp.width)
+        }
+        
+    }
+    
+    
+    
+}
--- MoiveGram/ViewController.swift
+++ MoiveGram/ViewController.swift
@@ -6,14 +6,191 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    let appTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "MovieGram"
+        label.textColor = .red
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 30, weight: .heavy)
+        
+        return label
+    }()
+    
+    let idTextField: UITextField = {
+        let tf = UITextField()
+        // NSAttributedString: 문자열에 관련된 속성(폰트, 문자 사이의 간격 등등)을 커스텀할 때 사용
+        tf.attributedPlaceholder = NSAttributedString(string: "이메일 주소 또는 전화번호", attributes: [.foregroundColor: UIColor.white])
+        tf.textAlignment = .center
+        tf.layer.cornerRadius = 8
+        tf.backgroundColor = .lightGray
+        
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [.foregroundColor: UIColor.white])
+        tf.textAlignment = .center
+        tf.layer.cornerRadius = 8
+        tf.backgroundColor = .lightGray
+        
+        return tf
+    }()
+    
+    let nicknameTextField: UITextField = {
+        let tf = UITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "닉네임", attributes: [.foregroundColor: UIColor.white])
+        tf.textAlignment = .center
+        tf.layer.cornerRadius = 8
+        tf.backgroundColor = .lightGray
+        
+        return tf
+    }()
+    
+    let countryTextField: UITextField = {
+        let tf = UITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "위치", attributes: [.foregroundColor: UIColor.white])
+        tf.textAlignment = .center
+        tf.layer.cornerRadius = 8
+        tf.backgroundColor = .lightGray
+        
+        return tf
+    }()
+    
+    let recommandCodeTextField: UITextField = {
+        let tf = UITextField()
+        tf.attributedPlaceholder = NSAttributedString(string: "추천코드", attributes: [.foregroundColor: UIColor.white])
+        tf.textAlignment = .center
+        tf.layer.cornerRadius = 8
+        tf.backgroundColor = .lightGray
+        
+        return tf
+    }()
+    
+    // let vs lazy var -> self 노란색 경고창 + UILifeCycle
+    lazy var joinButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("회원가입", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 18)
+        button.backgroundColor = .white
+        button.layer.cornerRadius = 8
+        button.addTarget(self, action: #selector(goHomeView), for: .touchUpInside)
+        
+        let b = ViewController.self // Meta Type
+        
+        return button
+    }()
+    
+    let addUserInfoButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("추가정보 입력", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 18)
+        
+        return button
+    }()
+    
+    let switchButton: UISwitch = {
+        let sw = UISwitch()
+        sw.onTintColor = .red
+        
+        
+        return sw
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        
+        configureHierarchy()
+        configureLayout()
+        // self.joinButton.addTarget(self, action: #selector(goHomeView), for: .touchUpInside)
     }
 
-
+    
+    func configureHierarchy() {
+        view.addSubview(appTitleLabel)
+        view.addSubview(idTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(nicknameTextField)
+        view.addSubview(countryTextField)
+        view.addSubview(recommandCodeTextField)
+        view.addSubview(joinButton)
+        view.addSubview(addUserInfoButton)
+        view.addSubview(switchButton)
+    }
+    
+    func configureLayout() {
+        appTitleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(44)
+        }
+        
+        idTextField.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(appTitleLabel.snp.bottom).offset(100)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(40)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(idTextField.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(40)
+        }
+        
+        nicknameTextField.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(40)
+        }
+        
+        countryTextField.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(40)
+        }
+        
+        recommandCodeTextField.snp.makeConstraints { make in
+            make.centerX.equalTo(view.snp.centerX)
+            make.top.equalTo(countryTextField.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(40)
+        }
+        
+        joinButton.snp.makeConstraints { make in
+            make.top.equalTo(recommandCodeTextField.snp.bottom).offset(16)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(60)
+        }
+        
+        addUserInfoButton.snp.makeConstraints { make in
+            make.top.equalTo(joinButton.snp.bottom).offset(12)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(24)
+        }
+        
+        switchButton.snp.makeConstraints { make in
+            make.top.equalTo(joinButton.snp.bottom).offset(12)
+            make.trailing.equalTo(view.safeAreaLayoutGuide).inset(24)
+        }
+    }
+    
+    
+    @objc func goHomeView() {
+        print(#function)
+        let vc = HomeViewController()
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
